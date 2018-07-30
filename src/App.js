bd03479ce989d662f1f21e5c36f09f12ff4981bc
zenius <zenius2050@gmail.com>
2018-07-30 19:37:24 +0530

    Caching functionality is added
    Caching functionality is added to make the application more efficient.

--- src/App.js
+++ src/App.js
@@ -23,9 +23,12 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: null,
+      results: null,
+      searchKey: "",
       searchTerm: DEFAULT_QUERY
     };
+
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
@@ -33,15 +36,24 @@ export default class App extends Component {
     this.removeItem = this.removeItem.bind(this);
   }
 
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
   setSearchTopStories(result) {
     const { hits, page } = result;
-    const oldHits = page != 0 ? this.state.result.hits : [];
+    const { searchKey, results } = this.state;
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
     const updatedHits = [...oldHits, ...hits];
-    this.setState({ result: { hits: updatedHits, page } });
+    this.setState({
+      results: { ...results, [searchKey]: { hits: updatedHits, page } }
+    });
   }
 
   componentDidMount() {
     const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
 
@@ -60,20 +72,29 @@ export default class App extends Component {
 
   onSearchSubmit(event) {
     const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.setState({ searchKey: searchTerm });
+
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
     event.preventDefault();
   }
 
   removeItem(itemToRemove) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
     const isNotId = item => item.objectID !== itemToRemove.objectID;
-    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
     this.setState({
-      result: { ...this.state.result, hits: updatedHits }
+      results: { ...results, [searchKey]: { hits: updatedHits, page } }
     });
   }
   render() {
-    const { searchTerm, result } = this.state;
-    const page = (result && result.page) || 0;
+    const { searchTerm, results, searchKey } = this.state;
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
+    const list =
+      (results && results[searchKey] && results[searchKey].hits) || [];
     return (
       <div className="page">
         <div className="interactions">
@@ -84,13 +105,14 @@ export default class App extends Component {
           >
             Search
           </Search>
-          {result && <Table list={result.hits} removeItem={this.removeItem} />}
+          <Table list={list} removeItem={this.removeItem} />
           {console.log(this.state)}
           <div className="interactions">
             <Button
-              onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}
+              onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
             >
               More
+              {/* this is for fetching for more list item from other pages */}
             </Button>
           </div>
         </div>
